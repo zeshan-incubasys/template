0dde7d7a25d8450bab652b1c365a13268b3b9585
Zeshan Hassan <zeshan.hassan1989@gmail.com>
2014-12-14 12:35:54 +0500

    removed extra files

--- include/head.php
+++ include/head.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Index Page</title>
-    <!--<link rel="stylesheet" href="<?=CSS_URL?>bootstrap.min.css">-->
-    <link rel="stylesheet" href="<?=CSS_URL?>bootstrap.css" media="all" type="text/css">
+    <link rel="stylesheet" href="<?=CSS_URL?>bootstrap.min.css" media="all" type="text/css">
     <link rel="stylesheet" href="<?=CSS_URL?>customize.css" media="all" type="text/css">
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script type="text/javascript">window.jQuery || document.write('<script src="<?=JS_URL?>jquery-1.11.1.min.js"><\/script>')</script>
